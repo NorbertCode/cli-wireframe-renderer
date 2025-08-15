301d6710c757c8b703f5a5494503cd8de8dbb8dd
Norbert <54719382+NorbertCode@users.noreply.github.com>
2025-08-15 16:22:52 +0200

    basic perspective projection function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+struct SpacePoint {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+struct Camera { 
+    // By default at position (0,0,0) and rotation (0,0,0)
+    focal_length: f64,
+}
+
+fn perspective_projection(point: &SpacePoint, camera: &Camera) -> SpacePoint {
+    let camera_coordinate_point = SpacePoint {
+        x: (camera.focal_length * point.x) / point.z,
+        y: (camera.focal_length * point.y) / point.z,
+        z: point.z,
+    };
+
+    camera_coordinate_point
+}
+
 fn main() {
-    println!("Hello, world!");
+    let camera = Camera {
+        focal_length: 1.0,
+    };
+
+    let point1 = SpacePoint {x: 1.0, y: 3.0, z: 3.0};
+    let screen_point1: SpacePoint = perspective_projection(&point1, &camera);
+
+    let point2 = SpacePoint {x: 1.0, y: 0.0, z: 3.0};
+    let screen_point2: SpacePoint = perspective_projection(&point2, &camera);
+
+    let point3 = SpacePoint {x: 1.0, y: 3.0, z: 6.0};
+    let screen_point3: SpacePoint = perspective_projection(&point3, &camera);
+
+    let point4 = SpacePoint {x: 1.0, y: 0.0, z: 6.0};
+    let screen_point4: SpacePoint = perspective_projection(&point4, &camera);
+
+    println!("({}, {})", screen_point1.x, screen_point1.y);
+    println!("({}, {})", screen_point2.x, screen_point2.y);
+    println!("({}, {})", screen_point3.x, screen_point3.y);
+    println!("({}, {})", screen_point4.x, screen_point4.y);
 }
